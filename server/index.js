645b1d259bbafb8d90f44a02dfe8efb624d6700e
nickmccartney <nmccartney.7@gmail.com>
2021-03-27 19:52:47 -0400

    add support for chart customizations to the api

--- server/index.js
+++ server/index.js
@@ -8,6 +8,21 @@ const querystring = require("querystring");
 const Alpaca = require("@alpacahq/alpaca-trade-api");
 
 require("dotenv").config();
+var dayjs = require("dayjs");
+
+const intervals = {
+  "1Min": 60000,
+  "5Min": 300000,
+  "15Min": 900000,
+  "1D": 86400000,
+};
+
+// convert date to unix Math.floor(eDate.getTime() / 1000)
+// to find number of candles that will be returned, subract sDate from eDate, divide by 1000, divide by interval = # of candles
+// if above 1000, need to make multiple requests
+// 
+
+
 
 // setting constants for alpaca to use down below if needed
 const alpaca = new Alpaca({
@@ -22,25 +37,26 @@ app.use(cors());
 app.get("/ticker/:id", (req, res) => {
   let parsedUrl = url.parse(req.originalUrl);
   let parsedQs = querystring.parse(parsedUrl.query);
-  console.log(parsedUrl);
-  console.log(parsedQs);
+  //console.log(parsedUrl);
+  //console.log(parsedQs);
   var ticker = req.params.id;
-  var limit = parsedQs.limit ? parsedQs.limit : "5Min"
-  var eDate = new Date(); // today
-  var sDate = new Date();
+  var limit = parsedQs.limit ? parsedQs.limit : "1000";
+  var interval = parsedQs.interval ? parsedQs.interval : "1Min"
+  var eDate = parsedQs.eDate ? parsedQs.eDate : new Date(); // today
+  var sDate = parsedQs.sDate ? parsedQs.sDate : new Date();
   sDate.setFullYear(sDate.getFullYear() - 1);
   var formattedData = [];
 
   alpaca
-    .getBars(limit, ticker, {
+    .getBars(interval, ticker, {
       start: sDate,
       end: eDate,
-      limit: 100,
+      limit: limit,
     })
     .then((data) => {
       data[ticker].map((candle) => {
         formattedData.push({
-          x: new Date(candle["startEpochTime"] * 1000).toLocaleString(),
+          x: dayjs(candle["startEpochTime"]*1000).format("MM/DD hh:mm"),
           y: [
             candle["openPrice"],
             candle["highPrice"],
@@ -49,6 +65,7 @@ app.get("/ticker/:id", (req, res) => {
           ],
         });
       });
+      console.log(formattedData)
       res.json(formattedData);
     });
 });
