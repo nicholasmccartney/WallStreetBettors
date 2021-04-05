d223bd07b8151b799042755cfeb7a7a06f3e40a9
stoltzen <stoltzen@wit.edu>
2021-04-05 13:35:25 -0400

    Datepicker and Slider added

--- src/pages/Homepage/tickerSearch.js
+++ src/pages/Homepage/tickerSearch.js
@@ -1,16 +1,35 @@
 import React from "react";
+import DatePicker from "react-datepicker";
+import Slider from '@material-ui/core/Slider';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       ticker: "",
+      date1: null,
+      date2: null,
+
     };
   }
 
   setTicker = (data) => {
     this.setState({
       ticker: data.target.value.toUpperCase(),
+    });
+  };
+
+  setDate1 = (data) => {
+    this.setState({
+      date1: data,
+    });
+  };
+
+  setDate2 = (data) => {
+    this.setState({
+      date2: data,
     });
   };
 
@@ -23,6 +42,33 @@ class Search extends React.Component {
             value={this.state.ticker}
             onChange={this.setTicker}
           />
+          
+          <select name="interval" id="1">
+            <option value="1Min">1Min</option>
+            <option value="5Min">5Min</option>
+            <option value="1Hr">1Hr</option>
+          </select>
+
+          <DatePicker selected={this.state.date1} 
+            onChange={this.setDate1} 
+            maxDate= {new Date()} 
+            isClearable 
+            placeholderText="Choose Start Date"/>
+
+          <DatePicker selected={this.state.date2} 
+            onChange={this.setDate2} 
+            maxDate= {new Date()} 
+            isClearable 
+            placeholderText="Choose End Date"/>
+
+          <Slider 
+            min={100}
+            max={1000}
+            defaultValue={100}
+            steps={10}
+            marks
+            />
+
         <input type="submit" value="Submit" />
       </form>
     );
