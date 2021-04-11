115b8c2611a97ec3d4998c46510392529cbc21a9
nickmccartney <nmccartney.7@gmail.com>
2021-04-11 14:52:38 -0400

    Update algorithms.js

--- src/pages/Homepage/algorithms.js
+++ src/pages/Homepage/algorithms.js
@@ -83,4 +83,75 @@ function getHistogram(data1, data2) {
   return histData;
 }
 
-export { getSMA , getCandleEMA, linetocandle, getMACD, getHistogram };
+// getCross shows when the sma5 and sma20 cross it then takes into
+// account the price and will buy or sell depending on previous action
+function getCross(arr, period) {
+  var sma20Single, sma5Single;
+  var cross, previousCross = false;
+  var buy = true;
+  var totalMoney = 0;
+  var test =[];
+  
+  // setting the universal array variables to sma5, and sma20 equal to 
+  // their respective calls
+  if (period === 5) sma5 = arr;
+  if (period === 20) sma20 = arr;
+
+  // will iterate through the length of the sma20 array (all of the data points)
+  for (var i = 0; i < sma20.length; i++) {
+
+    // This checks if the values are undefined incase we get 
+    // bad/missing data and just wont run anything in that case. 
+    if ((sma20[i] !== undefined && sma5[i] !== undefined)) {
+
+      //Setting objects equal to a single datapoint so the properties (x(date),y(price)) can be used
+      sma20Single = Object.values(sma20[i]);
+      sma5Single = Object.values(sma5[i]);
+      // Checks if the values arent null. Due to it being '20 data pt simple moving average' 
+      // before 20 data pts it is null
+      if (sma20Single[1] !== null && sma5Single[1] !== null) {
+
+        //if shorter (sma20) is greater than sma5 then its a buy signal
+        //We should then buy the stock even is sma20 starts out higher (this wouldnt matter with live trading)
+        //The cycle then continues aslong as it starts like this to buy whenever sma20 is higher then sma5
+        if (sma5Single[1] > sma20Single[1]) {
+          cross = true;
+        }
+        if (sma5Single[1] < sma20Single[1]) {
+          cross = false;
+        }
+
+        // First time a cross is set to true (sma5 > sma20) then it will signal a cross and a buy must happen
+        if (cross !== previousCross) {
+          // Then the previousCross signal is set to the same as the current cross signal it it can see 
+          // when a change happens the next time for it to run
+          previousCross = cross;
+          //console.log(`Lined Crossed at: ${sma5Single[0]}`);
+
+          if (buy === true) {
+            //totalMoney variable once something is bought it set = to the -purchase price
+            totalMoney -= sma5Single[1];
+            //console.log(`Buy! @ ${sma5Single[1]}`);
+            test.push({x: sma5Single[0],borderColor:'#1bfa44', label: {text: 'Buy'}, borderWidth: 3})
+            //datesBought = [sma5Single];
+            //boughtAndSoldMap[sma5Single[0]] = sma5Single[1];
+            buy = false;
+          } else {
+            // totamoney variable once something is sold is set = to the + sell price
+            totalMoney += sma5Single[1]
+            //console.log(`Sell! @ ${sma5Single[1]}`);
+            //console.log(`profit: $${totalMoney} per share`)
+            test.push({x: sma5Single[0],borderColor:'#de0408', label: {text: 'Sell'},borderWidth: 3})
+            //boughtAndSoldMap[sma5Single[0]] = sma5Single[1]
+            buy = true;
+          }
+        }
+      }
+    }
+  }
+  //console.log(test)
+  return test;
+}
+
+
+export { getSMA , getCandleEMA, linetocandle, getMACD, getHistogram, getCross};
